988ebfda28966a9c5fad5cccd62df3e05940aff5
Diego Alves <djalves424@gmail.com>
2023-12-09 01:16:58 -0300

    Import adjusted

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -29,11 +29,11 @@ INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Boo
 INSERT INTO tb_product (name, price, description, img_url) VALUES ('PC Gamer Foo', 4170.0, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/25-big.jpg');
 
 INSERT INTO tb_product_category (product_id, category_id) VALUES (1, 1);
-INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 1);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (3, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (4, 3);
-INSERT INTO tb_product_category (product_id, category_id) VALUES (5, 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (5, 2);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (6, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (7, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (8, 3);
