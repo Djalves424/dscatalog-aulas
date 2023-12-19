95799439be461ce5826df5b8a67dd86b82ccf0bf
djalves424 <djalves424@gmail.com>
2023-12-18 22:59:02 -0300

    Add BeforeEach

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.repositories;
 
 import com.devsuperior.dscatalog.entities.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,10 +15,15 @@ public class ProductRepositoryTests {
     @Autowired
     private ProductRepository repository;
 
+    private Long exitingId;
+
+    @BeforeEach
+    void setup() throws Exception {
+        exitingId = 1L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-
-        Long exitingId = 1L;
 
         repository.deleteById(exitingId);
 
